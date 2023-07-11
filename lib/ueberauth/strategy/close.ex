781d2f0ea0304adb99cd29ea80acb89e07e2627e
Taylor Sampson <taylor@savvycal.com>
2023-07-11 13:13:55 -0500

    Working to implement commented out Close functions

--- lib/ueberauth/strategy/close.ex
+++ lib/ueberauth/strategy/close.ex
@@ -32,27 +32,24 @@ defmodule Ueberauth.Strategy.Close do
   Handles the callback from Close.
   """
   def handle_callback!(%Plug.Conn{params: %{"code" => code}} = conn) do
-    # verifier = conn.cookies["fastmail_verifier"]
+    params = [
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: callback_url(conn)
+    ]
 
-    # params = [
-    #   grant_type: "authorization_code",
-    #   code: code,
-    #   code_verifier: verifier,
-    #   redirect_uri: callback_url(conn)
-    # ]
+    module = option(conn, :oauth2_module)
+    opts = [redirect_uri: callback_url(conn)]
 
-    # module = option(conn, :oauth2_module)
-    # opts = [redirect_uri: callback_url(conn)]
+    case module.get_token(params, opts) do
+      {:ok, %{token: %OAuth2.AccessToken{access_token: "" <> _string} = token}} ->
+        conn
+        |> put_private(:close_token, token)
+        |> fetch_user()
 
-    # case module.get_token(params, opts) do
-    #   {:ok, %{token: %OAuth2.AccessToken{access_token: "" <> _string} = token}} ->
-    #     conn
-    #     |> put_private(:fastmail_token, token)
-    #     |> fetch_user()
-
-    #   err ->
-    #     handle_failure(conn, err)
-    # end
+      err ->
+        handle_failure(conn, err)
+    end
   end
 
   @doc false
@@ -62,9 +59,8 @@ defmodule Ueberauth.Strategy.Close do
 
   @doc false
   def handle_cleanup!(conn) do
-    # conn
-    # |> put_private(:fastmail_user, nil)
-    # |> put_private(:fastmail_token, nil)
+    conn
+    |> put_private(:close_token, nil)
   end
 
   @doc """
@@ -173,34 +169,5 @@ defmodule Ueberauth.Strategy.Close do
   defp with_scope(opts, conn) do
     scope = conn.params["scope"] || option(conn, :default_scope)
     Keyword.put(opts, :scope, scope)
-  end
-
-  defp with_pkce_param(params, conn) do
-    # {verifier, challenge} = generate_pkce_challenge()
-
-    # conn = Plug.Conn.put_resp_cookie(conn, "fastmail_verifier", verifier, max_age: 60 * 10)
-
-    # {params
-    #  |> Keyword.put(:code_challenge_method, "S256")
-    #  |> Keyword.put(:code_challenge, challenge), conn}
-  end
-
-  # This generates a cryptographically random string for use in a PKCE
-  # challenge. This isn't quite perfect as it only uses 64 characters out of a
-  # possible 66, but it's good enough for our purposes.
-  #
-  # See https://tools.ietf.org/html/rfc7636#section-4.1
-  defp generate_pkce_challenge do
-    verifier =
-      :crypto.strong_rand_bytes(80)
-      |> Base.url_encode64()
-      |> String.trim_trailing("=")
-
-    challenge =
-      :crypto.hash(:sha256, verifier)
-      |> Base.url_encode64()
-      |> String.trim_trailing("=")
-
-    {verifier, challenge}
   end
 end
